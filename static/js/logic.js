5ec7141018d6f4cc914c62cb9314a5a4b3063a15
bripapp <brianna.papparotto@gmail.com>
2021-06-29 20:29:32 -0400

    create seeResults function

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,20 @@
-// outcome-btn.onclick = function(seeResults) { 
-//     myScript };  
+function seeResults() {
+    var genderField = document.getElementById('gender').value;
+    var ageField = document.getElementById('age').value;
+    var siblingField = document.getElementById('siblings').value;
+    var childField = document.getElementById('children').value;
+    var classField = document.getElementById('passClass').value;
 
-/* <a class="nav-link" href="comparison.html">Comparison</a> */
+    var result = document.getElementById('result');
+    
+    if (nameField.length < 3) {
+        result.textContent = 'Username must contain at least 3 characters';
+        //alert('Username must contain at least 3 characters');
+    } else {
+        result.textContent = 'Your username is: ' + nameField;
+        //alert(nameField);
+    }
+    }
+
+var subButton = document.getElementById('subButton');
+    subButton.addEventListener('click', getUserName, false); 
